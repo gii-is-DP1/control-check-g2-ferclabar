765657ad9c59f49ea59e89c11344524b6f0e0168
ferclabar <fernandoclarosb@gmail.com>
2022-01-19 16:12:41 +0100

    examen

--- src/main/java/org/springframework/samples/petclinic/feeding/Feeding.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/Feeding.java
@@ -2,16 +2,42 @@ package org.springframework.samples.petclinic.feeding;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.samples.petclinic.model.BaseEntity;
 import org.springframework.samples.petclinic.pet.Pet;
 
 import lombok.Getter;
 import lombok.Setter;
 
+
+@Entity
 @Getter
 @Setter
-public class Feeding {
-    Integer id;
+@Table(name = "feedings")
+public class Feeding extends BaseEntity{
+
+
+    @Column(name = "start_date")
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
+    @NotNull
     LocalDate startDate;
+
+    @DecimalMin("1")
+    @Column(name = "weeks_duration")
     double weeksDuration;
-    Pet pet;   
+
+    @ManyToOne(optional = false)
+    Pet pet;  
+    
+    @ManyToOne
+    @JoinColumn(name = "feeding_type_id", referencedColumnName = "id")
+    private FeedingType feedingType;
 }
--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingType.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingType.java
@@ -1,15 +1,38 @@
 package org.springframework.samples.petclinic.feeding;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import org.springframework.samples.petclinic.model.BaseEntity;
 import org.springframework.samples.petclinic.pet.PetType;
 
 import lombok.Getter;
 import lombok.Setter;
 
+
+
 @Getter
 @Setter
-public class FeedingType {
-    Integer id;
-    String name;
-    String description;
-    PetType petType;
+@Entity
+@Table(name = "feeding_types")
+
+public class FeedingType extends BaseEntity{
+
+    
+    @NotEmpty
+    @Size(min = 5, max = 50)
+    @Column(unique = true)
+    private String name;
+
+    @NotEmpty
+    private String description;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name= "pet_type_id", referencedColumnName = "id")
+    private PetType petType;
 }
